ee291ddf687fe984cf371b567ba81b7590ad150e
DlekiGh <DlekiYa@ayandex.ru>
2026-07-23 16:24:35 +0300

    clean stuff up

--- player.gd
+++
@@ -1,9 +0,0 @@
-class_name Player extends CharacterBody2D
-
-
-@export var speed: float = 300
-
-func _physics_process(_delta: float) -> void:
-	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
-	velocity = speed * direction
-	move_and_slide()
