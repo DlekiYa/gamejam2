3cba6479562ed6a341ed466cfe797eeb02a605b5
CoolerAtkam <roy.sucho2009@gmail.com>
2026-07-23 07:47:23 +0300

    :)

--- volcano_manager.gd
+++ volcano_manager.gd
@@ -1,11 +1,9 @@
 extends Node2D
 
-@export
-var volcano_textures: Array[Texture2D] # basically each index corresponds to a size texture
+@export var volcano_textures: Array[Texture2D] # basically each index corresponds to a size texture
 # now that I think about it, in the future we should assign scenes so that the collision shape is pre-made
-@export
-var my_sprite: Sprite2D
-var current_scale = 0
+@export var my_sprite: Sprite2D
+var current_scale: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,5 +11,5 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
